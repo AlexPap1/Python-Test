2fae424364c68976e432ddfe401b72d9c4184c4f
AlexPap <arpappagallo@gmail.com>
2023-02-14 21:08:57 -0500

    adding user model DB doesn't recognize your root user and password, need to grant priveleges

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -11,5 +11,5 @@ engine = create_engine(getenv('DB_URL'), echo=True, pool_size=20, max_overflow=0
 #manages connnection to DB
 Session = sessionmaker(bind=engine)
 #generates connections for CRUD
-Base = declarative_base
+Base = declarative_base()
 # map models to MySQL tables
