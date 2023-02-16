a9751cae5bff02ff423e55e9b8125549eb504daa
AlexPap <arpappagallo@gmail.com>
2023-02-15 20:14:55 -0500

    continued testing of error for seeds.py. Mysql shell error?

--- seeds.py
+++ seeds.py
@@ -4,3 +4,5 @@ from app.db import Session, Base, engine
 # drop and rebuild tables
 Base.metadata.drop_all(engine)
 Base.metadata.create_all(engine)
+
+## possible root of mysql error when running seeds.py in .env? (1045, "Access denied for user 'root'@'localhost' (using password: YES)")
